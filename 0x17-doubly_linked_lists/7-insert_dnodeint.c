b3833e5488b0d297b9201034da3695a99c13409e
DD1C74707 <gracevalley101@gmail.com>
2023-06-15 10:29:05 -0700

    Add task 8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,43 +9,46 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h;
-	dlistint_t *new_node = NULL;
+        dlistint_t *current = *h;
+        dlistint_t *new_node = NULL;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+        new_node = malloc(sizeof(dlistint_t));
+        if (new_node == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
+        new_node->n = n;
+        new_node->prev = NULL;
+        new_node->next = NULL;
 
-	if (idx == 0 && current != NULL)
-	{
-		new_node->next = (*h);
-		*h = new_node;
-		return (*h);
-	}
+        if (idx == 0 && current != NULL)
+        {
+                new_node->next = (*h);
+                if (*h != NULL)
+                {
+                    (*h)->prev = new_node;
+                }
+                *h = new_node;
+                return (new_node);
+        }
 
-	while (idx > 0 && current != NULL)
-	{
-		if (idx - 1 == 0)
-		{
-			new_node->prev = current;
-			new_node->next = current->next;
-			if (current->next != NULL)
-			{
-				current->next->prev = new_node;
-			}
-			current->next = new_node;
-			return (new_node);
-		}
-		current = current->next;
-		idx -= 1;
-	}
-	return (NULL);
+        while (idx > 0 && current != NULL)
+        {
+                if (idx - 1 == 0)
+                {
+                        new_node->prev = current;
+                        new_node->next = current->next;
+                        if (current->next != NULL)
+                        {
+                                current->next->prev = new_node;
+                        }
+                        current->next = new_node;
+                        return (new_node);
+                }
+                current = current->next;
+                idx -= 1;
+        }
+        return (NULL);
 }
-
 
