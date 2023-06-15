a47a2fb0e3826ae0a827d2c59ea42b2b300686cf
DD1C74707 <gracevalley101@gmail.com>
2023-06-15 10:05:08 -0700

    Add fucntion to insetnode at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a new node at given position
+ * @h: double pointer to head
+ * @idx: index to insert into
+ * @n: value to store in new node
+ * Return: Address of new node, or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = *h;
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (idx == 0 && current != NULL)
+	{
+		new_node->next = (*h);
+		*h = new_node;
+		return (*h);
+	}
+
+	while (idx > 0 && current != NULL)
+	{
+		if (idx - 1 == 0)
+		{
+			new_node->prev = current;
+			new_node->next = current->next;
+			if (current->next != NULL)
+			{
+				current->next->prev = new_node;
+			}
+			current->next = new_node;
+			return (new_node);
+		}
+		current = current->next;
+		idx -= 1;
+	}
+	return (NULL);
+}
+
