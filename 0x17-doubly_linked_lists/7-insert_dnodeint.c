928ecbc936b517989626c45633b87a8a3c5635a0
DD1C74707 <gracevalley101@gmail.com>
2023-06-19 08:37:38 -0700

    Add task 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,60 +9,45 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h;
-	dlistint_t *new_node;
-	unsigned int i = 0;
+	unsigned int c;
+	dlistint_t *tmp, *prev, *new;
 
-	if (idx == 0 && *h != NULL)
-	{
-		new_node = create_node(n);
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->next = *h;
-		if (*h != NULL)
-		{
-			(*h)->prev = new_node;
-		}
-		*h = new_node;
-		return (new_node);
-	}
-	while (current != NULL && i < idx - 1)
-	{
-		current = current->next;
-		i++;
-	}
-	if (current != NULL)
-	{
-		new_node = create_node(n);
-		new_node->prev = current;
-		new_node->next = current->next;
-		if (current->next != NULL)
-		{
-			current->next->prev = new_node;
-		}
-		current->next = new_node;
-		return (new_node);
-	}
-	return (NULL);
-}
-
-/**
- * create_node - insert a new node at given position
- * @n: value to store in new node
- * Return: Address of new node, or NULL if failed
- */
-dlistint_t *create_node(int n)
-{
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-	{
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	for (tmp = *h, c = 1; tmp && c < idx; c++, tmp = tmp->next)
+		prev = tmp;
+	if (idx == 0)
+	{
+		*h = new; new->prev = NULL;
+		new->next = (tmp == NULL) ? NULL : tmp;
+		return (new);
 	}
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	return (new_node);
+	if (idx == 1)
+	{
+		prev = *h;
+		tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
+		new->prev = prev; new->next = tmp; prev->next = new;
+		if (tmp)
+			tmp->prev = new;
+		return (new);
+	}
+	if (idx == c && tmp == NULL)
+	{
+		if (prev != NULL)
+		{
+			new->prev = prev; new->next = NULL;
+			prev->next = new; return (new);
+		}
+		free(new); return (NULL);
+	}
+	else if (idx != c && tmp == NULL)
+	{
+		free(new); return (NULL);
+	}
+	prev = tmp; tmp = tmp->next; new->prev = prev;
+	new->next = tmp; prev->next = new; tmp->prev = new;
+	return (new);
 }
+
