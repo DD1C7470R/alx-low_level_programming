113f5aeb3c168143b04a0f17cf78f59d1015156f
DD1C74707 <gracevalley101@gmail.com>
2023-06-19 12:06:59 -0700

    Add operations.so file

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sub of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: div of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod od  two numbers
+ * @a: first number
+ * @b: second number
+ * Return: mod of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
