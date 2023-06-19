ec9ada0d27aa25c4ede96d06ff9ea1a33056f3a1
DD1C74707 <gracevalley101@gmail.com>
2023-06-19 14:45:39 -0700

    Change library name to libmask

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/DD1C7470R/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.so
-export LD_PRELOAD=/tmp/101-make_me_win.so
+wget -P /tmp https://github.com/DD1C7470R/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
