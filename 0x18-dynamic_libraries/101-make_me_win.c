127e0344e4b1ea96dadabb43174ffe529727ac71
DD1C74707 <gracevalley101@gmail.com>
2023-06-19 21:46:08 -0700

    Add new libmask

--- 0x18-dynamic_libraries/101-make_me_win.c
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * check_result - checks results from the draw
  * Returns: 1
  */
-int check_result()
+int check_result(int *matrix, int param1, int param2)
 {
-	_puts("Congratulations, you win the Jackpot!");
+	printf("Congratulations, you win the Jackpot!\n");
 	return (1);
 }
