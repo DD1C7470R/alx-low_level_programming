399b82fa46fbef192f21b6dba6d7e01306357788
DD1C74707 <gracevalley101@gmail.com>
2023-07-07 07:27:47 -0700

    ADd description for betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+
+/**
+ * key_index - implementation of the key index
+ * @key: string used to generate hash value
+ * @size: size of table
+ * Return: an index a unsigned long int
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
