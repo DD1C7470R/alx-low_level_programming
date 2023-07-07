da72e24c446c7887f70dfc3f7444ae6de0a911b9
DD1C74707 <gracevalley101@gmail.com>
2023-07-07 10:01:09 -0700

    Add search algorith

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_print - Print a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx;
+	int count;
+	hash_node_t *current;
+
+	printf("{");
+	if (ht != NULL)
+	{
+		for (idx = 0, count = 0; idx < ht->size; idx++)
+		{
+			current = ht->array[idx];
+
+			if (current != NULL)
+			{
+				if (count == 1)
+					printf(", ");
+				printf("'%s': '%s'", current->key, current->value);
+
+				while (current->next != NULL)
+				{
+					printf(", ");
+					printf("'%s': '%s'", current->key, current->value);
+
+					current->next = current->next;
+				}
+				count = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
+
+
+
