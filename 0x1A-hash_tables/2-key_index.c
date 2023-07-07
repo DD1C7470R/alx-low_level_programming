c9e5278d265ffd2b6ba8c64bbd379fffc161c560
DD1C74707 <gracevalley101@gmail.com>
2023-07-07 07:24:40 -0700

    Add tas 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+	unsigned long int index;
+
+	if (size == 0 || strlen((const char *)key) == 0)
+		return (0);
+
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return (index);
+}
+
