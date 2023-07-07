4fde2ffa727e0531dc0f456c667ea55a2253ef30
DD1C74707 <gracevalley101@gmail.com>
2023-07-07 11:01:30 -0700

    Add task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete given hash table
+ * @ht: hash table given
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, *prev;
+	unsigned long int N , idx;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return;
+	}
+
+	for (idx = 0, N = ht->size; idx < N; idx++)
+	{
+		tmp = ht->array[idx];
+		while (tmp)
+		{
+			prev = tmp;
+			tmp = tmp->next;
+			free(prev->key);
+			free(prev->value);
+			free(prev);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
+
