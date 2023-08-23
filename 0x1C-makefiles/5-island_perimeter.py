5687a4d2c1b67ff1521a1da322c8e095ee79d778
DD1C74707 <gracevalley101@gmail.com>
2023-08-23 07:44:14 -0700

    Add task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+5-main
+"""
+
+
+def island_perimeter(grid):
+    """
+    Defines the perimeter of a given grid
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                # Check adjacent cells and reduce perimeter accordingly
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
