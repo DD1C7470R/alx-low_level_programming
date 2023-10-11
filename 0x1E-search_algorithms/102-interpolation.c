8417c98953536285e14666027ef6db5b43feb44e
DD1C74707 <gracevalley101@gmail.com>
2023-10-10 20:25:57 -0700

    Use recursion for interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,41 +1,49 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - Performs a linear search for a key value
- * @array: Array to search value
- * @size: size of input array
- * @value: value to search
- * Return: 1 if found and -1 if not found
+ * interpol_search_rec - Recursive interpolation search
+ * @array: Array to search in
+ * @low: Starting index of the subarray
+ * @high: Ending index of the subarray
+ * @value: Value to search
+ * @size: Size of the array
+ * Return: Index of the found value or -1 if not found
+ */
+int interpol_search_rec(int *array, int low, int high, int value, size_t size)
+{
+	if (low <= high && value >= array[low] && value <= array[high])
+	{
+		int pos = low + ((double)(high - low)
+			/ (array[high] - array[low])) * (value - array[low]);
+
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+
+		if (array[pos] < value)
+			return (interpol_search_rec(array, pos + 1, high, value, size));
+
+		return (interpol_search_rec(array, low, pos - 1, value, size));
+	}
+
+	printf("Value checked array[%d] is out of range\n", low);
+
+	return (-1);
+}
+
+/**
+ * interpolation_search - Initiates interpolation search
+ * @array: Array to search in
+ * @size: Size of the array
+ * @value: Value to search
+ * Return: Index of the found value or -1 if not found
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	int low = 0;
-	int high = size - 1;
-	int mid;
-
-	if (!array)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	while (
-		(array[high] != array[low]) && (value >= array[low])
-		&& (value <= array[high])
-	)
-	{
-		mid = low + ((value - array[low])
-				* (high - low) / (array[high] - array[low]));
-		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
-
-		if (array[mid] < value)
-			low = mid + 1;
-		else if (value < array[mid])
-			high = mid - 1;
-		else
-			return (mid);
-	}
-
-	if (value == array[low])
-		return (low);
-	else
-		return (-1);
+	return (interpol_search_rec(array, 0, size - 1, value, size));
 }
 
